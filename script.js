555690560d30b1f60900afdbd1947f651d39f508
merlina23 <126361540+merlina23@users.noreply.github.com>
2023-09-23 21:59:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+
+document.getElementById("color").addEventListener('click', function() {   
+  document.body.style.backgroundColor = "pink";
+  });
+  
+  
+
+  document.getElementById("reestablecer").addEventListener('click', function() {   
+    document.body.style.backgroundColor = "whitesmoke";
+  });
+
+
+
+
+    
+
+
+
+  
